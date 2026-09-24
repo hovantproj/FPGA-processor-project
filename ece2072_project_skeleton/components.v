7a79fb454ac393dc3886dd9f6bb206dfde6c3112
Philo Lee <philolee07@gmail.com>
2026-09-24 16:23:16 +1000

    edited task 1.3 switched shift direction

--- ece2072_project_skeleton/components.v
+++ ece2072_project_skeleton/components.v
@@ -86,6 +86,7 @@ module multiplexer(SignExtDin, R0, R1, R2, R3, R4, R5, R6, R7, G, sel, Bus);
 	end
 endmodule
 
+
 module ALU (input_a, input_b, alu_op, result);
 	 
 	// This module computes arithmetic result from input_a and input_b based on alu_op
@@ -104,11 +105,11 @@ module ALU (input_a, input_b, alu_op, result);
 			3'b010: result = input_a - input_b;
 			3'b011:
 				if(input_a[15]) begin
-					result = $signed(input_b) <<< (-input_a); // https://electronics.stackexchange.com/questions/132773/difference-between-and-in-verilog
+					result = $signed(input_b) >>> (-input_a); // https://electronics.stackexchange.com/questions/132773/difference-between-and-in-verilog (ask jess next prac)
 				end
 				
 				else begin
-					result = $signed(input_b) >>> input_a;
+					result = $signed(input_b) <<< input_a;
 				end
 			
 			default: result = 16'b0000;
